15ce3fc562cfa723a5b211f91b914fbe9c5a897e
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2025-04-12 00:12:38 +0530

    Delete medi directory

--- medi/src/Data/api.js
+++
@@ -1,4 +0,0 @@
-// api.js
-
-export const GOOGLE_SCRIPT_GetUserRoomInfo = 'https://script.google.com/macros/s/AKfycbwZMGM-6VAI9DdMdmwfHsIJjdQwvJmJ7u17vQu0LN9yKFHGZhe7TusXARGdrMqwBJVt0A/exec';
-export const GOOGLE_SCRIPT_GetUserRoomInfo_v2 = 'https://script.google.com/...';  // Example of a new URL or endpoint
